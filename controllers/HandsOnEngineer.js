b7fd352bb34ef68e0f2f31d67a085c67cc386b9f
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2023-12-15 20:50:53 +0200

    Update HandsOnEngineer endpoints

--- controllers/HandsOnEngineer.js
+++ controllers/HandsOnEngineer.js
@@ -3,14 +3,24 @@
 var utils = require('../utils/writer.js');
 var HandsOnEngineer = require('../service/HandsOnEngineerService');
 
-module.exports["changeStatus"] = function changeStatus(req, res, next, body, userID, proposalID) {
-  HandsOnEngineer["changeStatus"](body, userID, proposalID)
+module.exports.changeStatus = function changeStatus(req, res, next, body, userID, proposalID) {
+  HandsOnEngineer.changeStatus(body, userID, proposalID)
     .then(function (response) {
       utils.writeJson(res, response);
     })
     .catch(function (response) {
 
 
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getHandsOnEngineerProposal = function getHandsOnEngineerProposal (req, res, next, userID, proposalID) {
+  HandsOnEngineer.getHandsOnEngineerProposal(userID, proposalID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
       utils.writeJson(res, response);
     });
 };
