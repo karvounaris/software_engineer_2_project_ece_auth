7d5be7123567ef4afc482f28568ecabd3cce303f
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2023-12-15 20:56:23 +0200

    Update EngineerService

--- service/EngineerService.js
+++ service/EngineerService.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var EngineerProposalData =  null;
 
 /**
  * Uploads a file containing data from the sensors
@@ -26,23 +26,49 @@ exports.postFile = function(body,userID) {
  **/
 exports.postProposal = function(body,userID) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "newValue" : 2.3021358869347655,
-  "prposalID" : 6,
-  "partID" : 1,
-  "description" : "description",
-  "id" : 0,
-  "title" : "title",
-  "userID" : 5,
-  "currentValue" : 5.637376656633329,
-  "status" : "status"
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
+    EngineerProposalData = {
+      newValue : body.newValue,
+      prposalID : body.prposalID,
+      partID : body.partID,
+      description : body.description,
+      id : body.id,
+      title : body.title,
+      userID : body.userID,
+      currentValue : body.currentValue,
+      status : body.status,
+      confirmation: body.confirmation
+    };
+    if (EngineerProposalData){
+      resolve(EngineerProposalData); // Resolve with the constructed dynamic response
     } else {
       resolve();
     }
   });
 }
 
+exports.getEngineerProposal = function(userID, proposalID) {
+  return new Promise(function(resolve, reject) {
+    if (EngineerProposalData) {
+      // If updatedData is available, resolve with it
+      resolve(EngineerProposalData);
+    } else {
+      // If updatedData is not available, handle it accordingly (resolve with default or an error)
+      resolve();
+    }
+  });
+};
+
+
+/**
+ * View the file containing data from the sensors
+ * Returns a file containing data from the sensors based on a single ID
+ *
+ * body Object 
+ * userID Integer This is the unique identifier of the user
+ * no response value expected for this operation
+ **/
+exports.viewFile = function(userID) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
